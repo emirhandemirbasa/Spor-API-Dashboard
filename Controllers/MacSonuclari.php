4abddf909b2862eedfc67c6eaf07595b861416ab
Emirhan Demirbaşa <emirhanlife@gmail.com>
2026-01-29 16:59:52 +0300

    feature: Seçilen lige ait maç sonuçlarını getiren servis kodlandı.

---
+++ Controllers/MacSonuclari.php
@@ -0,0 +1,31 @@
+<?php
+require "./config/config.php";
+$curl = curl_init();
+$lig = $_POST["ligler"] ?? "super-lig";
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.collectapi.com/sport/results?league=$lig",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: apikey $api",
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  //echo $response;
+}
+$macsonuclari = json_decode($response,true);
+?>
